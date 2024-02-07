01b8aef634cbb7b0ee773a20802c71ebe0c9ab69
Andrew Lambert <hg9958@wayne.edu>
2024-02-07 10:34:29 -0500

    Add server.js file

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the Student Planner API!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

e40737a097b36a168b5e3d90e419ca1fdffe659e
Andrew Lambert <hg9958@wayne.edu>
2024-02-21 11:25:29 -0500

    Added to-do-list

--- frontend/src/pages/todolist.js
+++ frontend/src/pages/todolist.js
@@ -3,7 +3,35 @@ import '../stylesheets/todolistpagestyles.css';
 
 export default function Todolist() {
     return (
-        <div className='home-background'>
-    </div>
+        <div className='todolist-grid'>
+            <div className ="title-div">
+                <p className = "title-text">
+                    TO DO LIST
+                </p>
+            </div>
+            <div className ="todolist-mainbody">
+                <div className="todolist-background">
+                    <div className="clipboard"></div>
+                    <div className="clipboard-circle"></div>
+                    <div className ="Shadow-effect">
+                        <div className ="center-title">
+                            <p className="todolist-title">
+                            TO DO
+                            </p>
+                        </div>
+                        <div className="List">
+                            <ul>
+                                <li>List event 1</li>
+                                <li>List event 2</li>
+                                <li>List event 3</li>
+                                <li>List event 4</li>
+                                <li>List event 5</li>
+                                <li>List event 6</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }

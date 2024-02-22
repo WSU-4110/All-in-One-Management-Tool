99f776e6d61576b1eaca9b79d132329d63bca70d
Istiaque Hossain <hi7498@wayne.edu>
2024-02-22 16:17:16 -0500

    new changes-profile and settings pages

--- frontend/src/pages/settings.js
+++ frontend/src/pages/settings.js
@@ -2,10 +2,27 @@ import '../stylesheets/settingspagestyles.css';
 import Footer from '../components/Footer';
 import Header from '../components/header';
 
-
 export default function Settings() {
+    // Sample settings options
+    const settingsOptions = [
+        { id: 1, title: "Change Password" },
+        { id: 2, title: "Notification Preferences" },
+        { id: 3, title: "Theme Settings" },
+        { id: 4, title: "Language Preferences" }
+    ];
+
     return (
         <div className='home-background'>
-    </div>
-    )
+            <Header />
+            <div className='settings-container'>
+                <h1>Settings</h1>
+                <ul>
+                    {settingsOptions.map(option => (
+                        <li key={option.id}>{option.title}</li>
+                    ))}
+                </ul>
+            </div>
+            <Footer />
+        </div>
+    );
 }

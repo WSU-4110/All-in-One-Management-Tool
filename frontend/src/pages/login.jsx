e09a3b80d620724d098312a261bde3130c96248d
Omar Almasri <omaralmasri100@gmail.com>
2024-03-20 17:49:01 -0400

    Integrated MongoDB into the Login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,154 +1,193 @@
 import React, { useState } from "react"
+import { Link , useNavigate } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from "react-bootstrap/Col";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import Alert from "react";
 import '../stylesheets/backgroundstyles.css';
-
-async function setSessionVariables(username, password){
-    sessionStorage['Username'] = username;
-    sessionStorage['Password'] = password;
-}
 
 const Login = (props) => {
     const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [verifiedUsername, setVerifiedUsername] = useState(false);
     const [usernameError, setUsernameError] = useState('');
     const [verifiedPassword, setVerifiedPassword] = useState(false);
     const [passwordError, setPasswordError] = useState('');
+    const [alertVisible, setAlertVisible] = useState(false);
+    const [alertError, setAlertError] = useState('');
+    const [data, setData] = useState([]);
     const navigate = useNavigate();
-
+    
     async function handleSubmit(e) {
         e.preventDefault();
         console.log(username);
         console.log(password);
-        var data = {}
-        var goodUsername = verifyUsername();
-        var goodPassword = verifyPassword();
+
+        async function fetchData() {
+            const response = await fetch(`http://localhost:5050/`);
+            if (!response.ok) {
+                setAlertVisible(true);
+                const message = `An error has occurred: ${response.statusText}`;
+                setAlertError(message);
+                console.log(message)
+                return;
+            }
+            // Looks to see if username exists in the database
+            const record = await response.json();
+            for (let i = 0; i < record.length; i++) {
+                if (record[i].username === username) {
+                    setData(record[i]);
+                    break;
+                }
+            }
+            if (!record) {
+                setAlertVisible(true);
+                setAlertError(`Record with username ${username} not found`);
+                console.log(`Record with username ${username} not found`);
+                navigate("/");
+                return;
+            }
+            setData(record);
+        }
 
         if (verifyUsername() && verifyPassword()) {
-            data = {
-                username: username,
-                password: password
+            fetchData();
+            if (data.password === password.toString()) {
+                navigate("/home");
+            } else {
+                setAlertVisible(true);
+                setAlertError("Incorrect username or password.");
+                console.log("Incorrect username or password");
             }
-            setSessionVariables(username, password);
-            console.log('Username:', data.username)
-            console.log('password:', data.password)
-            navigate('/home');
             return;
         }
     }
 
     function verifyUsername() {
         if (username === '') {
-            setVerifiedUsername(true);
+            setVerifiedUsername(false);
             setUsernameError('Please enter a username');
             return false;
         }
-        if ((!(username.includes('@'))) && (!(username.includes('.'))) && (username.includes(' '))
-            && (!(username.endsWith('.com') || username.endsWith('.net')
-            || username.endsWith('.org') || username.endsWith('.edu')
-            || username.endsWith('.gov')))) {
-            setVerifiedUsername(true);
+        if (username.includes(' ')) {
+            setVerifiedUsername(false);
             setUsernameError('Please enter a valid username');
             return false;
-        } else {
-            setVerifiedUsername(false);
-            setUsernameError('');
-            return true;
         }
+        setVerifiedUsername(true);
+        setUsernameError('');
+        return true;
     }
+
+    // function verifyEmail() {
+    //     if (email === '') {
+    //         setVerifiedEmail(true);
+    //         setEmailError('Please enter a email');
+    //         return false;
+    //     }
+    //     if ((!(email.includes('@'))) && (!(email.includes('.'))) && (email.includes(' '))
+    //         && (!(email.endsWith('.com') || email.endsWith('.net')
+    //         || email.endsWith('.org') || email.endsWith('.edu')
+    //         || email.endsWith('.gov')))) {
+    //         setVerifiedEmail(true);
+    //         setEmailError('Please enter a valid email');
+    //         return false;
+    //     } else {
+    //         setVerifiedEmail(false);
+    //         setEmailError('');
+    //         return true;
+    //     }
+    // }
 
     function verifyPassword() {
         if (password === '') {
-            setVerifiedPassword(true);
+            setVerifiedPassword(false);
             setPasswordError('Please enter a password');
             return false;
         } else {
-            setVerifiedPassword(false);
+            setVerifiedPassword(true);
             setPasswordError('');
             return true;
         }
     }
     
     return(
-    <div className='init-background'>
-        <div
-        style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-        }}>
-            <Form noValidate>
-                <h1
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>Login</h1>
-                <br></br>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label 
+        <div className='init-background'>
+            <div
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <Form noValidate>
+                    <h1
                     style={{
-                        color: "white",
-                        textShadow: "2px 2px 4px #000000",
-                    }}>Username: </Form.Label>
-                    <Form.Control
-                    type="username"
-                    placeholder="Username"
-                    value={username}
-                    isInvalid={verifiedUsername}
-                    onChange={(u) => setUsername(u.target.value)}/>
-                    <Form.Control.Feedback type="invalid"
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>Login</h1>
+                    <br></br>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label 
                         style={{
                             color: "white",
-                            textShadow: "2px 2px 4px #FF0000",
-                        }}>
-                        {usernameError}
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <br></br>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label 
-                    style={{
-                        color: "white",
-                        textShadow: "2px 2px 4px #000000",
-                    }}>Password: </Form.Label>
-                    <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    isInvalid={verifiedPassword}
-                    onChange={(p) => setPassword(p.target.value)}/>
-                    <Form.Control.Feedback type="invalid"
+                            textShadow: "2px 2px 4px #000000",
+                        }}>Username: </Form.Label>
+                        <Form.Control
+                        type="username"
+                        placeholder="Username"
+                        value={username}
+                        isInvalid={!verifiedUsername}
+                        onChange={(u) => setUsername(u.target.value)}/>
+                        <Form.Control.Feedback type="invalid"
+                            style={{
+                                color: "white",
+                                textShadow: "2px 2px 4px #FF0000",
+                            }}>
+                            {usernameError}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <br></br>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label 
                         style={{
                             color: "white",
-                            textShadow: "2px 2px 4px #FF0000",
-                        }}>
-                        {passwordError}
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <br></br>
-                <Col
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
-                    <Button variant="primary" type="submit"
-                    className="mx-1" onClick={handleSubmit}>
-                        Login
-                    </Button>
-                    <Link to="/signup" className="mx-1">
-                        <Button variant="primary">Sign Up</Button>
-                    </Link>
-                </Col>
+                            textShadow: "2px 2px 4px #000000",
+                        }}>Password: </Form.Label>
+                        <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        isInvalid={!verifiedPassword}
+                        onChange={(p) => setPassword(p.target.value)}/>
+                        <Form.Control.Feedback type="invalid"
+                            style={{
+                                color: "white",
+                                textShadow: "2px 2px 4px #FF0000",
+                            }}>
+                            {passwordError}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <br></br>
+                    <Col
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                        <Button variant="primary" type="submit"
+                        className="mx-1" onClick={handleSubmit}>
+                            Login
+                        </Button>
+                        <Link to="/signup" className="mx-1">
+                            <Button variant="primary">Sign Up</Button>
+                        </Link>
+                    </Col>
 
-            </Form>
+                </Form>
+            </div>
         </div>
-    </div>
     )
 }
 
--- server/routes/record.js
+++ server/routes/record.js
@@ -12,17 +12,25 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 
 // This section will help you get a list of all the records.
-
 router.get("/", async (req, res) => {
   let collection = await db.collection("records");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
 
-// router.get("/", async (req, res) => {
-//   let collection = await db.collection("records");
-//   let results = await collection.find({}).toArray();
-//   res.send(results).status(200);
+// // This section will help you get a single record by username
+// router.get("/:username", async (req, res) => {
+//   try {
+//     let collection = await db.collection("records");
+//     let query = { username: req.params.username };
+//     let result = await collection.findOne(query);
+
+//     if (!result) res.send("Not found").status(404);
+//     else res.send(result).status(200);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Error retrieving record");
+//   }
 // });
 
 // This section will help you get a single record by id

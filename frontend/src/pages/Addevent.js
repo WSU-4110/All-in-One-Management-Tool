c51680de215fe59c2680e745911a878d1c68fdaa
Omar Almasri <omaralmasri100@gmail.com>
2024-03-21 13:25:36 -0400

    Styling changes
    Made some styling changes and added some features to the Addevent and todolist page

--- frontend/src/pages/Addevent.js
+++ frontend/src/pages/Addevent.js
@@ -1,13 +1,22 @@
 import '../stylesheets/addeventstyles.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from "react-bootstrap/Col";
 
 export default function Addevent() {
     const [getClass, setClass] = useState("");
     const [getAssignment, setAssignment] = useState("");
     const [getDueDate, setDueDate] = useState("");
     const [getDescription, setDescription] = useState("");
+    const [validClass, setValidClass] = useState(true);
+    const [validAssignment, setValidAssignment] = useState(true);
+    const [validDueDate, setValidDueDate] = useState(true);
+
+    const nav = useNavigate();
 
     function changeClass(e) {
         e.preventDefault();
@@ -29,26 +38,52 @@ export default function Addevent() {
         setDescription(e.target.value);
     }
 
-    function getEventInfo() {
-        let Info = JSON.parse(localStorage.getItem('eventInfo'));
-
-        if (Info === null) {
-            Info = [];
+    function checkForEmpty() {
+        if (getClass === "") {
+            setValidClass(false);
+        } else {
+            setValidClass(true);
         }
+        if (getAssignment === "") {
+            setValidAssignment(false);
+        } else {
+            setValidAssignment(true);
+        }
+        if (getDueDate === "") {
+            setValidDueDate(false);
+        } else {
+            setValidDueDate(true);
+        }
+        if (getClass === "" || getAssignment === "" || getDueDate === "") {
+            return false;
+        }
+        return true;
+    }
 
-       const newEvent = 
-        {
-            class: getClass,
-            Assignment: getAssignment,
-            DueDate: getDueDate,
-            Description: getDescription,
+    async function getEventInfo(e) {
+        e.preventDefault();
+        if (checkForEmpty()) {
+            let Info = JSON.parse(localStorage.getItem('eventInfo'));
 
-        };
-        
-        Info.push(newEvent);
-        localStorage.setItem('eventInfo', JSON.stringify(Info));
+            if (Info === null) {
+                Info = [];
+            }
 
-        console.log(Info);
+            const newEvent = 
+            {
+                class: getClass,
+                Assignment: getAssignment,
+                DueDate: getDueDate,
+                Description: getDescription,
+
+            };
+            
+            Info.push(newEvent);
+            localStorage.setItem('eventInfo', JSON.stringify(Info));
+
+            console.log(Info);
+            nav('/todolist');
+        }
     }
 
     return (
@@ -57,7 +92,111 @@ export default function Addevent() {
                 <div className="home-inner">
                     <Header />
                     <div className='Addevent-mainbody'>
-                        <div className = 'Addevent-title'>
+                        <Form noValidate>
+                        <h1
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>Add Event</h1>
+                        <br></br>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label 
+                            style={{
+                                color: "white",
+                            }}>Class: </Form.Label>
+                            <Form.Control
+                            type="input"
+                            placeholder="Class"
+                            value={getClass}
+                            isInvalid={!validClass}
+                            onChange={(u) => setClass(u.target.value)}/>
+                            <Form.Control.Feedback type="invalid"
+                                style={{
+                                    color: "red",
+                                    textShadow: "2px 2px 4px #000000",
+                                }}>
+                                Please enter a class
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <br></br>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label 
+                            style={{
+                                color: "white",
+                            }}>Assignment: </Form.Label>
+                            <Form.Control
+                            type="input"
+                            placeholder="Assignment"
+                            value={getAssignment}
+                            isInvalid={!validAssignment}
+                            onChange={(p) => setAssignment(p.target.value)}/>
+                            <Form.Control.Feedback type="invalid"
+                                style={{
+                                    color: "red",
+                                    textShadow: "2px 2px 4px #000000",
+                                }}>
+                                Please enter an assignment
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <br></br>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label 
+                            style={{
+                                color: "white",
+                            }}>Due Date: </Form.Label>
+                            <Form.Control
+                            type="date"
+                            placeholder="Due Date"
+                            value={getDueDate}
+                            isInvalid={!validDueDate}
+                            onChange={(p) => setDueDate(p.target.value)}/>
+                            <Form.Control.Feedback type="invalid"
+                                style={{
+                                    color: "red",
+                                    textShadow: "2px 2px 4px #000000",
+                                }}>
+                                Please enter a due date
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <br></br>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label 
+                            style={{
+                                color: "white",
+                            }}>Description: </Form.Label>
+                            <Form.Control
+                            type="input"
+                            placeholder="Description"
+                            value={getDescription}
+                            onChange={(p) => setDescription(p.target.value)}/>
+                        </Form.Group>
+                        <p style={{
+                            color: "white",
+                            fontSize: "0.8em",
+                            marginTop: "0.3em",
+                        }}>*Description not required</p>
+                        <br></br>
+                        <Col
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                            <Button variant="primary" type="submit"
+                            className="mx-1" onClick={getEventInfo}>
+                                Submit
+                            </Button>
+                        </Col>
+                    </Form>
+
+
+
+
+
+
+
+                        {/* <div className = 'Addevent-title'>
                             <p className = "Addevent-text">ADD EVENT</p>
                         </div>
                         <div className = "form-grid"> 
@@ -96,11 +235,11 @@ export default function Addevent() {
                                     SUBMIT
                                 </button>
                             </div>
-                            </div>
                         </div>
-                    </div>
-                    <Footer />
+                    </div> */}
                 </div>
+                <Footer />
+            </div>
         </div>
     )
 }
--- frontend/src/pages/todolist.js
+++ frontend/src/pages/todolist.js
@@ -5,10 +5,18 @@ import Footer from '../components/Footer';
 import checkmarkimage from '../images/checkmark.png';
 import trashicon from '../images/trashicon.png';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
  
 export default function Todolist() {
     const [todos, setTodos] = useState([]);
     const Eventobj = JSON.parse(localStorage.getItem('eventInfo'));
+    const [containsEvents, setContainsEvents] = useState(true);
+    const navigate = useNavigate();
+
+    if (Eventobj === null) {
+        setContainsEvents(false);
+    }
 
     useEffect(() => {
         const eventInfo = JSON.parse(localStorage.getItem('eventInfo')) || [];
@@ -25,34 +33,43 @@ export default function Todolist() {
     return (
         <div className='home-outer'>
             <div className='home-background'> Hello</div>
-            <div className='home-inner'>
-                <Header />
-                <div className='todolist-grid'>
-                    <div className ="title-div">
-                        <p className = "title-text">
-                            TO DO LIST 
-                        </p>
-                </div>
-                <div className='list'>
-                    {Eventobj.map((value, index) => {
-                        return <div key = {index} className='list-item'>
-                            <div className='left-side'> 
-                                <img className='checkmark-image' src={checkmarkimage} />
-                                <div className='Assignment-section'>
-                                    <p className='assignment-text'> {value.Assignment} </p>
-                                    <p className='class-text'> {value.class} </p>
-                                    <p className='date-text'> {value.DueDate} </p>
+                <div className='home-inner'>
+                    <Header />
+                    <div className='todolist-grid'>
+                        <div className ="title-div">
+                            <p className = "title-text">
+                                TO DO LIST 
+                            </p>
+                    </div>
+                    <div className='list'>
+                        {containsEvents && Eventobj.map((value, index) => {
+                            return <div key = {index} className='list-item'>
+                                <div className='left-side'> 
+                                    <img className='checkmark-image' src={checkmarkimage} />
+                                    <div className='Assignment-section'>
+                                        <p className='assignment-text'> {value.Assignment} </p>
+                                        <p className='class-text'> {value.class} </p>
+                                        <p className='date-text'> {value.DueDate} </p>
+                                    </div>
+                                </div>
+
+                                <div className='right-side'>
+                                <button onClick={() => removeItem(index)}className='trash-button'>
+                                    <img className='trash-icon' src={trashicon} />
+                                    </button>
                                 </div>
                             </div>
-
-                            <div className='right-side'>
-                            <button onClick={() => removeItem(index)}className='trash-button'>
-                                <img className='trash-icon' src={trashicon} />
-                                </button>
-                            </div>
-                        </div>
-                    })}
-                </div>
+                        })}
+                    </div>
+                    <div className='add-event-button'
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    
+                    }}>
+                        <Button variant="primary" onClick={() => navigate('/addevent')}>Add Event</Button>
+                    </div>
                 </div>
             <Footer />
                 

e3e1d8d3b94a5672f29eb35ca83607d5fd11b5ce
Uvejs <gerguve0@gmail.com>
2024-03-28 16:36:48 -0400

    notification bell

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,14 +2,18 @@ import React from "react";
 import '../stylesheets/headerstyles.css'
 import NoficicationIcon from '../images/NotificationIcon.png';
 import ProfilePicture from '../images/ProfilePicture.png';
-//import SideBar from '../images/SideBar.png';
+import SideBar from '../images/SideBar.png';
 import {Link} from 'react-router-dom'
 
+
 const Header = () => {
+    const notisCount = JSON.parse(sessionStorage.getItem('Tasks')).length;
+
     return (
         <header>
             <div className="flexbox-header">
                 <div className="left-side">
+                    <img className = "Sidebar-Image" src={SideBar} alt="sidebar"/>
                     <p className="Header-Text">Student Planner</p>
                 </div>
                         
@@ -17,6 +21,8 @@ const Header = () => {
                 <Link to ="/notifications" className="header-link">
                     <img className ="notification-icon" src={NoficicationIcon} alt="Notification-Icon" />
                     <p className="icon-label">NOTIFICATION</p> {/* Label for Notification */}
+                    <div className='stupid-red-dot'>{notisCount}</div>
+
                 </Link>
                 <Link to ="/profile" className="header-link">
                     <img className = "profile-picture" src={ProfilePicture} alt="Profile-Picture" />

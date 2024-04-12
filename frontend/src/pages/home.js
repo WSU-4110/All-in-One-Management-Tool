64ef22f6ddaec94f4769da4d69aeac8f9514cb28
Carter Rock <Carterrock49@gmail.com>
2024-04-11 21:48:17 -0400

    Test Successful this time for sure

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -56,7 +56,7 @@ export default function Home() {
                                 </span> 
                                 <Link to="/addevent" className="link-style">
                                 
-                                        
+                                    ADD EVENT
                                         
                                 </Link>
                             </div>

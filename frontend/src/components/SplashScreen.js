2f436088e4e227ca8668db0cc0dbcc37237e0df4
Istiaque Hossain <hi7498@wayne.edu>
2024-04-18 15:43:20 -0400

    minor font changes

--- frontend/src/components/SplashScreen.js
+++ frontend/src/components/SplashScreen.js
@@ -5,7 +5,7 @@ import splashImage from '../images/splash.png'
 const SplashScreen = () => {
     return (
       <div className="splash-screen">
-            <h1 style={{ color: 'black', fontSize: '2rem' }}>Welcome!</h1>
+            <h1 style={{ color: 'black', fontSize: '3rem' }}>Welcome!</h1>
             <img src={splashImage} alt="Splash" />
             <h2 style={{ color: 'black', fontSize: '3rem' }}>All in One Management Tool</h2>
             <h3>Designed by Andrew Lambert, Carter Rock, Faiza Lasker, Istiaque Hossain, Omar Almasri, Uvejs Gerguri</h3>

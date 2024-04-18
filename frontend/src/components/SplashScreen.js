d125ca5554477bcdd63014515153ba5b87bcae45
Istiaque Hossain <hi7498@wayne.edu>
2024-04-18 15:01:15 -0400

    Added a splash screen

---
+++ frontend/src/components/SplashScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../stylesheets/splashscreen.css'
+import splashImage from '../images/splash.png'
+
+const SplashScreen = () => {
+    return (
+      <div className="splash-screen">
+            <h1 style={{ color: 'black', fontSize: '2rem' }}>Welcome!</h1>
+            <img src={splashImage} alt="Splash" />
+            <h2 style={{ color: 'black', fontSize: '3rem' }}>All in One Management Tool</h2>
+            <h3>Designed by Andrew Lambert, Carter Rock, Faiza Lasker, Istiaque Hossain, Omar Almasri, Uvejs Gerguri</h3>
+        </div>
+    );
+  };
+
+  export default SplashScreen;
